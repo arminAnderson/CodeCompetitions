4860fcaba9916b588c2cbc4d145065ce190064be
Armin Anderson <arminkanderson@hotmail.com>
2020-10-06 16:41:45 -0700

    59

--- ProjectEuler/XorDecrypt.py
+++ ProjectEuler/XorDecrypt.py
@@ -1,10 +1,40 @@
 f = open("inputFiles/XorDecrypt.txt", "r")
-nums = f.read().split(",")
-key = 0
-index = 0
+nums = f.read().strip().split(',')
+sequence = [int(c) for c in nums]
 
-key = "aaa"
-print((key))
-for n in nums:
-    c = int(n) ^ key
-    #print(chr(c))
+def Final(arr, key):
+    ret = 0
+    r = ''
+    for i in range(0, len(arr)):
+        letter = (chr(arr[i]^int(key[i%len(key)])))
+        r += letter
+        ret += ord(letter)
+
+    print(r)
+    print(ret)
+
+def f(arr, key):
+    s = ''
+    a = ''
+    b = ''
+    c = ''
+    d = ''
+    for i in range(0, len(arr)):
+        s = a
+        a = b
+        b = c
+        c = d
+        d = chr(arr[i]^int(key[i%len(key)]))
+        if (a+b+c == "and" or a+b+c =="the") and (d == ' ' and s == ' '):
+            return True
+    return False
+
+while(True):
+    for a in range(26):
+        for b in range(26):
+            for c in range(26):
+                if f(sequence, [a + 97, b + 97, c + 97]):
+                    print("{} {} {}".format(chr(a + 97), chr(b + 97), chr(c + 97)))
+                    Final(sequence, [a + 97, b + 97, c + 97])
+                    exit()
+
